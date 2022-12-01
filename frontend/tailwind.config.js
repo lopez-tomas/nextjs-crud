ddaf93bc62c09b8d58c4f569728db4bfebba3b99
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-11-30 21:18:08 -0300

    [FEAT/tailwind]: new colors and spacings added.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,12 +8,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "white-color": "var(--white)",
-        "black-color": "var(--black)",
-        "primary-color": "var(--primary-color)",
-        "secondary-color": "var(--secondary-color)",
-        "green-color": "var(--green)",
-        "red-color": "var(--red)",
+        'white-color': 'var(--white)',
+        'black-color': 'var(--black)',
+        'black-2-color': 'var(--black-2)',
+        'green-color': 'var(--green)',
+        'red-color': 'var(--red)',
+        'primary-color': 'var(--primary-color)',
+        'secondary-color': 'var(--secondary-color)',
+        'tertiary-color': 'var(--tertiary-color)',
+        'fourth-color': 'var(--fourth-color)',
+        'auxiliar-color': 'var(--auxiliar-color)',
+      },
+      spacing: {
+        '10vh': '10vh',
+        '20vh': '20vh',
+        '80vh': '80vh',
+        '90vh': '90vh',
       }
     },
   },
