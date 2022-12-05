b5e90e8329dc791d38d7e19e0fc0aa38b7bccfd5
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-12-05 17:25:01 -0300

    [STYLES/view]: Home page updated.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,13 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">
-        Hello World!
-      </h1>
-    </div>
+    <>
+      <div className='w-full p-4 bg-gray-200'>
+        <h1 className='text-md text-gray-700'>
+          {'>'} <span className='text-2xl'>admin@admin</span>
+        </h1>
+      </div>
+    </>
   )
 }
