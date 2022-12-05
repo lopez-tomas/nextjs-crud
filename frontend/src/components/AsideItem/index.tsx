021ae2a73088940e1ae1b80cafd2795155c146f5
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-12-05 15:10:06 -0300

    [FEAT]: using Link component from Next to navigate.

--- frontend/src/components/AsideItem/index.tsx
+++ frontend/src/components/AsideItem/index.tsx
@@ -1,24 +1,28 @@
 import { NextPage } from 'next'
+import Link from 'next/link';
 
 interface Props {
-  item: { name: string; icon: React.ReactNode };
+  item: { name: string; path: string; icon: React.ReactNode };
 }
 
 const AsideItem: NextPage<Props> = ({ item }) => {
   return (
-    <li
-      className='
-        flex
-        items-center
-        w-full
-        p-2
-        rounded-md
-        hover:bg-tertiary-color
-        hover:text-white-color
-        hover:cursor-pointer
-    '>
-      <span className='mr-2'>{item.icon}</span>
-      {item.name}
+    <li>
+      <Link
+        href={item.path}
+        className='
+          flex
+          items-center
+          w-full
+          p-2
+          rounded-md
+          hover:bg-tertiary-color
+          hover:text-white-color
+          hover:cursor-pointer
+      '>
+        <span className='mr-2'>{item.icon}</span>
+        {item.name}
+      </Link>
     </li>
   )
 }
