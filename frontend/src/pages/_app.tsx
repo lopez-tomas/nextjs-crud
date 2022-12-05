57e4870ab10c685af1ae6725d589f7a565258dfe
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-12-05 16:41:38 -0300

    [FEAT]: using Head component from Next.

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import Layout from '@/components/Layout'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>admin | by Tomás</title>
+        <meta name='description' content='admin - Tomás López' />
+        {/* <link rel='icon' href='' /> */}
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
