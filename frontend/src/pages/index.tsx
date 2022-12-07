7be82203e9bb02a9d52983fe9d6ca1dddfbf6084
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-12-07 19:01:00 -0300

    [FIX/styles]: div height set to 90vh

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <>
-      <div className='w-full p-4 bg-gray-200'>
+      <div className='h-90vh w-full p-4 bg-gray-200'>
         <h1 className='text-md text-gray-700'>
           {'>'} <span className='text-2xl'>admin@admin</span>
         </h1>
