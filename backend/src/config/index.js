fe066e42880b1a4313ae21829b9e8d8b00ee5514
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-12-07 17:23:51 -0300

    [FEAT]: database config created using env variables.

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -6,6 +6,15 @@ const config = {
   port: process.env.PORT || process.env.LOCAL_PORT,
 }
 
+const dbConfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+}
+
 export {
   config,
+  dbConfig,
 }
