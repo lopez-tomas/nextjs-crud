e846654f4ab8f53b5307be5938fad74c79208ebf
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-12-08 18:33:28 -0300

    [FEAT]: using categoriesRouter.

--- backend/src/api/routes/index.js
+++ backend/src/api/routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import { router as productsRouter } from '#routes/products.router.js'
+import { router as categoriesRouter } from '#routes/categories.router.js'
 
 const router = (app) => {
   const router = Router()
   app.use('/api', router)
 
   router.use('/products', productsRouter)
+  router.use('/categories', categoriesRouter)
 }
 
 export {
