2e04d880bbe0bc5969583145920a2cd5831737d1
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-12-08 18:36:25 -0300

    [FIX/styles]: overflow-y added.

--- frontend/src/containers/PageContainer/index.tsx
+++ frontend/src/containers/PageContainer/index.tsx
@@ -9,7 +9,7 @@ interface Props {
 const PageContainer: React.FC<Props> = ({ pretitle, title, children }) => {
   return (
     <>
-      <div className='h-90vh w-full p-4 bg-gray-200'>
+      <div className='h-90vh w-full overflow-y-auto p-4 bg-gray-200'>
         <h1 className='text-md text-gray-700'>
           {pretitle} <span className='text-2xl'>{title}</span>
         </h1>
