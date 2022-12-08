8726ab87ffdc12752fc6774d47ec438c09b5b89f
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-12-08 14:46:58 -0300

    [FEAT/service]: new method getProduct(id) created.

--- backend/src/api/services/products.service.js
+++ backend/src/api/services/products.service.js
@@ -21,6 +21,27 @@ class ProductsService {
       })
     })
   }
+
+  getProduct(id) {
+    return new Promise((resolve, reject) => {
+      const sql = `
+        SELECT
+          productos.*,
+          categorias.categoria AS nombre_categoria
+        FROM productos
+        LEFT JOIN categorias ON categorias.id = productos.id_categoria
+        WHERE 1 = 1
+        AND productos.id = ?
+      `
+
+      const query = connection.query(sql, [id], (err, results, fields) => {
+        if (err)
+          return reject(boom.badRequest('[getProduct] - Error al obtener el producto', err))
+
+        resolve(JSON.parse(JSON.stringify(results[0])))
+      })
+    })
+  }
 }
 
 export default ProductsService
