10bb3353c33b57a8af376f70a127901286685f23
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-12-08 18:37:16 -0300

    [STYLES]: using Button component. Minor changes added.

--- frontend/src/pages/productos/index.tsx
+++ frontend/src/pages/productos/index.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link'
 import PageContainer from '@/containers/PageContainer'
+import Button from '@/components/Button'
 import Table from '@/components/Table'
 import TableRow from '@/components/Table/Row'
-import { IProduct } from '../../types'
+
+import { IProduct } from 'src/types'
 import { NextPage } from 'next'
 import { FaPlus } from 'react-icons/fa'
 
@@ -14,40 +16,32 @@ const ProductsPage: NextPage<Props> = ({ products }) => {
   return (
     <PageContainer pretitle='/' title='Productos'>
       <div className='my-6 px-2 py-4 bg-white-color rounded-md drop-shadow-xl'>
-          <Link href='/producto'>
-            <button className='
-              flex
-              items-center
-              p-2
-              bg-primary-color
-              text-white-color
-              rounded-md
-              hover:bg-tertiary-color
-            '>
-              <FaPlus className='mr-1' /> Nuevo
-            </button>
-          </Link>
-        </div>
+        <Link href='/producto'>
+          <Button text='Nuevo' primary>
+            <FaPlus className='mr-1' />
+          </Button>
+        </Link>
+      </div>
 
-        <div className='bg-white-color rounded-md'>
-          {products?.length > 0
-              ?
-                <Table columns={['COD', 'NOMBRE', 'CATEGORÍA', 'STOCK']}>
-                  {products?.map(product => (
-                    <TableRow key={product.id} item={product} />
-                  ))
-                  }
-                </Table>
-              :
-                <h1>No se encontaron registros.</h1>
-          }
-        </div>
+      <div className='bg-white-color rounded-md'>
+        {products?.length > 0
+          ?
+            <Table columns={['COD', 'NOMBRE', 'CATEGORÍA', 'STOCK']}>
+              {products?.map(product => (
+                <TableRow key={product.id} item={product} />
+              ))
+              }
+            </Table>
+          :
+            <h1>No se encontaron registros.</h1>
+        }
+      </div>
     </PageContainer>
   )
 }
 
 export async function getServerSideProps() {
-  const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL!)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/products`)
   const data = await res.json()
 
   return {
