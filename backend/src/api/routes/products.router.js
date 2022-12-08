442b41ab76be3669ea1a2993c3dcc977e542600f
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-12-08 18:34:46 -0300

    [FEAT]: using CategoriesService to obtain ALL categories when user wants a product data.

--- backend/src/api/routes/products.router.js
+++ backend/src/api/routes/products.router.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import ProductsService from '#services/products.service.js'
+import CategoriesService from '#services/categories.service.js'
 
 const router = express.Router()
-const service = new ProductsService()
+const productService = new ProductsService()
+const categoryService = new CategoriesService()
 
 router.get('/', async (req, res) => {
   try {
-    const response = await service.getProducts()
+    const response = await productService.getProducts()
     res.status(200).json({ products: response })
   } catch (error) {
     res.status(500).json({ code: error.data.code, message: error.message })
@@ -15,8 +17,13 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try {
-    const response = await service.getProduct(req.params.id)
-    res.status(200).json({ product: response })
+    const response = await productService.getProduct(req.params.id)
+    const categories = await categoryService.getCategories()
+
+    res.status(200).json({
+      product: response,
+      categories: categories
+    })
   } catch (error) {
     res.status(500).json({ code: error.data.code, message: error.message })
   }
