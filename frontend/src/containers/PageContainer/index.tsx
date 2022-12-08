edf4117be18fb302ebed7a37f91db7dac0602676
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-12-08 14:55:22 -0300

    [FEAT/containers]: new container PageContainer created.

---
+++ frontend/src/containers/PageContainer/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+interface Props {
+  pretitle: string;
+  title: string;
+  children?: React.ReactNode | React.ReactNode[];
+}
+
+const PageContainer: React.FC<Props> = ({ pretitle, title, children }) => {
+  return (
+    <>
+      <div className='h-90vh w-full p-4 bg-gray-200'>
+        <h1 className='text-md text-gray-700'>
+          {pretitle} <span className='text-2xl'>{title}</span>
+        </h1>
+
+        {children}
+      </div>
+    </>
+  )
+}
+
+export default PageContainer
