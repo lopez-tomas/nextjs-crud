e19a5ff8f6d58ad277e5cba36949a1e99474fd5d
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-12-12 21:24:28 -0300

    [FEAT]: getProduct endpoint updated. updateProduct endpoint created.

--- backend/src/api/routes/products.router.js
+++ backend/src/api/routes/products.router.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import ProductsService from '#services/products.service.js'
-import CategoriesService from '#services/categories.service.js'
 
 import { validatorHandler } from '#middlewares/validator.handler.js'
 import {
@@ -11,7 +10,6 @@ import {
 
 const router = express.Router()
 const productService = new ProductsService()
-const categoryService = new CategoriesService()
 
 router.get('/', async (req, res) => {
   try {
@@ -22,19 +20,21 @@ router.get('/', async (req, res) => {
   }
 })
 
-router.get('/:id', async (req, res) => {
-  try {
-    const response = await productService.getProduct(req.params.id)
-    const categories = await categoryService.getCategories()
+router.get('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res) => {
+    const { id } = req.params
+    try {
+      const response = await productService.getProduct(id)
 
-    res.status(200).json({
-      product: response,
-      categories: categories
-    })
-  } catch (error) {
-    res.status(500).json({ code: error.data.code, message: error.message })
+      res.status(200).json({
+        product: response,
+      })
+    } catch (error) {
+      res.status(500).json({ code: error.data.code, message: error.message })
+    }
   }
-})
+)
 
 router.post('/',
   validatorHandler(createProductSchema, 'body'),
@@ -44,6 +44,20 @@ router.post('/',
     try {
       const newProduct = await productService.createProduct(body)
       res.status(201).json(newProduct)
+    } catch (error) {
+      res.status(500).json({ code: error.code, message: error.message })
+    }
+  }
+)
+
+router.patch('/',
+  validatorHandler(updateProductSchema, 'body'),
+  async (req, res) => {
+    const body = req.body
+
+    try {
+      const response = await productService.updateProduct(body)
+      res.status(202).json(response)
     } catch (error) {
       res.status(500).json({ code: error.code, message: error.message })
     }
