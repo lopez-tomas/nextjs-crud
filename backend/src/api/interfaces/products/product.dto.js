b77f9d9a8a8fcfce12b36e40cd4394a9b5c4e361
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-12-13 20:50:23 -0300

    [FEAT]: col1, col2, col3, col4 and col5 allow null. Making optional some schema values.

--- backend/src/api/interfaces/products/product.dto.js
+++ backend/src/api/interfaces/products/product.dto.js
@@ -4,11 +4,11 @@ const id = Joi.number().integer()
 const id_category = Joi.number().integer()
 const name = Joi.string().max(100)
 const description = Joi.string()
-const col1 = Joi.string().max(1000)
-const col2 = Joi.string().max(1000)
-const col3 = Joi.string().max(1000)
-const col4 = Joi.string().max(1000)
-const col5 = Joi.string().max(1000)
+const col1 = Joi.string().max(1000).allow(null)
+const col2 = Joi.string().max(1000).allow(null)
+const col3 = Joi.string().max(1000).allow(null)
+const col4 = Joi.string().max(1000).allow(null)
+const col5 = Joi.string().max(1000).allow(null)
 const active = Joi.number().min(0).max(1)
 const featured = Joi.number().min(0).max(1)
 
@@ -21,28 +21,28 @@ const createProductSchema = Joi.object({
   id_category: id_category.required(),
   name: name.required(),
   description: description.required(),
-  col1: col1,
-  col2: col2,
-  col3: col3,
-  col4: col4,
-  col5: col5,
+  col1: col1.optional(),
+  col2: col2.optional(),
+  col3: col3.optional(),
+  col4: col4.optional(),
+  col5: col5.optional(),
   active: active.required(),
-  featured: featured,
+  featured: featured.optional(),
 })
 
 const updateProductSchema = Joi.object({
   id: id.required(),
-  name: name,
-  id_category: id_category,
-  name: name,
-  description: description,
-  col1: col1,
-  col2: col2,
-  col3: col3,
-  col4: col4,
-  col5: col5,
-  active: active,
-  featured: featured,
+  name: name.optional(),
+  id_category: id_category.optional(),
+  name: name.optional(),
+  description: description.optional(),
+  col1: col1.optional(),
+  col2: col2.optional(),
+  col3: col3.optional(),
+  col4: col4.optional(),
+  col5: col5.optional(),
+  active: active.optional(),
+  featured: featured.optional(),
 })
 
 export {
