d386c43d2f2e0d2e4ba9c5580029dfd83a756dc7
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-12-15 20:02:44 -0300

    [FEAT/service]: LoginService created.

---
+++ backend/src/api/services/login.service.js
@@ -0,0 +1,36 @@
+import { connection } from '#database/index.js'
+import boom from '@hapi/boom'
+import bcrypt from 'bcryptjs'
+
+class LoginService {
+
+  getUser(username, password) {
+    return new Promise((resolve, reject) => {
+      const sql = `
+        SELECT
+          usuarios.*
+        FROM usuarios
+        WHERE 1 = 1
+        AND usuarios.usuario = ?
+      `
+
+      const query = connection.query(sql, [username], async (err, results, fields) => {
+        if (err) {
+          return reject(boom.badRequest('[getUser] - Error al obtener el usuario', err))
+        }
+
+        const user = JSON.parse(JSON.stringify(results[0]))
+        const { password: userPassword, ...userData} = user
+
+        const passwordMatched = await bcrypt.compare(password, userPassword)
+        if (!passwordMatched) {
+          return reject(boom.unauthorized('[getUser] - El usuario y/o contraseña no coinciden.', err))
+        }
+
+        resolve(JSON.parse(JSON.stringify(userData)))
+      })
+    })
+  }
+}
+
+export default LoginService
