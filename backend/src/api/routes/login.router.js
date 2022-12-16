06fe36ae184a06fd147ee0e338c34c459db1fa56
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-12-16 17:37:52 -0300

    minor changes added.

--- backend/src/api/routes/login.router.js
+++ backend/src/api/routes/login.router.js
@@ -8,19 +8,20 @@ const router = express.Router()
 const service = new LoginService()
 
 router.post('/', async (req, res, next) => {
-  const { username, password } = req.body
-
   try {
+    const { username, password } = req.body
+
     const user = await service.getUser(username, password)
     const token = jwt.sign({ id: user.id }, secretKey, { expiresIn: '1d' })
 
     res.cookie('jwt', token, {
+      path: '/',
       httpOnly: true,
       secure: config.env === 'production',
       maxAge: 24 * 60 * 60 * 1000 // 1 day
     })
 
-    res.status(200).json({ user })
+    res.status(200).json({ user, token })
   } catch (err) {
     next(err)
   }
