3329e98d8139a62a48623b89aeb78df53cf40729
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-12-16 17:34:17 -0300

    [FIX]: tokenAuthorization middleware fixed.

--- backend/src/api/middlewares/authorization.handler.js
+++ backend/src/api/middlewares/authorization.handler.js
@@ -1,8 +1,15 @@
 import boom from '@hapi/boom'
+import jwt from 'jsonwebtoken'
 import { secretKey } from '#config/index.js'
 
 const tokenAuthorization = (req, res, next) => {
-  const token = req.cookies['jwt']
+  let token = ''
+  if (req.cookies.jwt) {
+    token = req.cookies.jwt
+  } else {
+    token = req.headers.cookies
+  }
+
   if (!token) {
     return next(boom.unauthorized('No token provided'))
   }
